cfac13f5316fd6550a17726c92e673a2a77e5c99
tysprouse <tsprouse@ucsd.edu>
2022-05-01 19:19:00 -0700

    created test file

--- skilldemo.java
+++ skilldemo.java
@@ -1,1 +1,5 @@
-
+public class skilldemo{
+    public static int add(int x, int y){
+        return x + y;
+    }
+}
---
+++ skilldemotester.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class skilldemotester {
+    
+    @Test
+    public void testAdd() {
+        assertEquals(2, skilldemo.add(1,2));
+    }
+}
